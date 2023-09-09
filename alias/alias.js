c2c843910f01a815c5c051f0d6e1f860a2ee06a0
Andraelja <andraeljaprama@gmail.com>
2023-09-09 23:14:35 +0700

    first commit

---
+++ alias/alias.js
@@ -0,0 +1,11 @@
+const andra = "Andra Elja Prama";
+
+function sum(first, second){
+    return first + second;
+}
+
+class Person {
+
+}
+
+export {andra, sum, Person}
---
+++ object-module/object-module.js
@@ -0,0 +1,14 @@
+export const firstName = "Andra";
+export const middleName = "Elja";
+export const lastName = "Prama";
+
+export function hello(name){
+    console.log(`Hello ${name}`)
+}
+
+export class Person {
+    constructor(){
+        this.name = "Nayla";
+    }
+}
+
